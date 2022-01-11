5d96590c92756e934633c1659536160b98b25ad8
italosilva98 <italosilvestre3@icloud.com>
2022-01-10 21:42:08 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,4 +13,12 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://semfoto.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z',TIMESTAMP WITH TIME ZONE '2021-07-14T10:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-08-14T10:00:00Z',TIMESTAMP WITH TIME ZONE '2021-08-14T10:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'https://google.com',1,'http://external.link',1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Trilha Angular','Trilha principal do curso',2,'https://google.com',0,'http://external.link',1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('FORUM','Trilha principal do curso',3,'https://google.com',2,'http://external.link',1);
